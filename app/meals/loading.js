c501bd9e6e007c6d4007f7ba9e460a153a75bb84
Nutt S. <webdes20th@gmail.com>
2024-01-30 22:22:48 +0700

    2024030 : Start Chapter 111

---
+++ app/meals/loading.js
@@ -0,0 +1,5 @@
+import classes from './loading.module.css'
+
+export default  function MealLoadingPage() {
+    return <p className={classes.loading}>Fetching meals ...</p>
+}
--- app/meals/page.js
+++ app/meals/page.js
@@ -2,26 +2,33 @@ import classes from './page.module.css'
 import Link from "next/link";
 import MealsGrid from "../../components/meals/meals-grid";
 import {getMeals} from "../../lib/meals";
+import {Suspense} from "react";
+
+async function Meals() {
+    const meals = await getMeals()
+    return <MealsGrid meals={meals}/>
+}
 
 export default async function MealsPage() {
-    const meals = await  getMeals()
 
     return (<>
-            <header className={classes.header}>
-                <h1>
-                    Delicious meals, created <span>by you</span>
-                </h1>
-                <p>
-                    Choose your favorite recipe and cook it yourself
-                </p>
-                <p className={classes.cta}>
-                    <Link href={"/meals/share"}>
-                        Share Your Favorite Recipe
-                    </Link>
-                </p>
-            </header>
-            <main className={classes.main}>
-                <MealsGrid meals={meals} />
-            </main>
-        </>)
+        <header className={classes.header}>
+            <h1>
+                Delicious meals, created <span>by you</span>
+            </h1>
+            <p>
+                Choose your favorite recipe and cook it yourself
+            </p>
+            <p className={classes.cta}>
+                <Link href={"/meals/share"}>
+                    Share Your Favorite Recipe
+                </Link>
+            </p>
+        </header>
+        <main className={classes.main}>
+            <Suspense fallback={<p className={classes.loading}>Fetching meals ...</p>}>
+                <Meals/>
+            </Suspense>
+        </main>
+    </>)
 }
