18949b5243ee5afe1adb872d063a30ee03c5040c
Nutt Sawaddiyanont <nuttsawaddiyanont@Nutts-MacBook-Pro.local>
2024-02-22 22:48:14 +0700

    update : 20240222

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,3 +1,36 @@
-export default function MealDetailPage() {
-    return <h1>Meal Details</h1>
+import {getMeal} from "/lib/meals";
+import Image from 'next/image';
+import classes from "./page.module.css";
+import {notFound} from "next/navigation";
+
+export default function MealDetailPage({params}) {
+
+    const meal = getMeal(params.mealSlug)
+
+    if (!meal) {
+        notFound();
+    }
+    meal.instructions = meal.instructions.replace(/\n/g, '<br />')
+
+    return (<>
+        <header className={classes.header}>
+            <div className={classes.image}>
+                <Image src={meal.image} alt={meal.title} fill/>
+            </div>
+            <div className={classes.headerText}>
+                <h1>{meal.title}</h1>
+                <p className={classes.creator}>
+                    by <a href={`mailto:${meal.creator_email}`}>NAME</a>
+                </p>
+                <p className={classes.summary}>
+                    SUMMARY
+                </p>
+            </div>
+        </header>
+        <main>
+            <p className={classes.instructions} dangerouslySetInnerHTML={{
+                __html: meal.instructions
+            }}></p>
+        </main>
+    </>)
 }
