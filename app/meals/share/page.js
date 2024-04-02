be1ad865534db29c32a2acc277ba135f7ddd0c67
Nutt <webdes20th@gmail.com>
2024-04-02 12:58:48 +0700

    start chapter 124

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -4,6 +4,9 @@ import classes from './page.module.css';
 import ImagePicker from "../../../components/meals/image-picker";
 import {shareMeal} from "../../../lib/actions";
 
+import {useFormStatus} from 'react-dom'
+import MealsFormSubmit from "../../../components/meals/meals-form-submit";
+
 export default function ShareMealPage() {
 
     return (<>
@@ -44,7 +47,7 @@ export default function ShareMealPage() {
                 </p>
                 <ImagePicker label={"Your Image"} name="image"/>
                 <p className={classes.actions}>
-                    <button type="submit">Share Meal</button>
+                    <MealsFormSubmit />
                 </p>
             </form>
         </main>
